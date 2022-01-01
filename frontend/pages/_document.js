a27a7ed25edc808243495bbdffeddeed2f50b45a
Alice Liang <aliceyliang@gmail.com>
2022-01-01 16:46:02 -0500

    initial styling

---
+++ frontend/pages/_document.js
@@ -0,0 +1,24 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=PT+Sans+Caption:wght@700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,15 +2,41 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import { v4 as uuidv4 } from 'uuid'
 import { Container } from '../components/Container'
+import Image from 'next/image'
+
 
 const Home: NextPage = () => {
   return (
     <Container>
-      <h1 style={{ fontSize: '4rem' }}>koi polloi</h1>
 
+      <div style = {{ display: 'flex', flexFlow: 'row wrap', alignItems: 'center'}}> 
+
+        <div style = {{ display: 'block'}}>
+        <Image src="/lily_with_flower.png" alt="koi" width="140" height="140" layout="intrinsic"/>
+        </div>
+
+        <h1 style={{ fontSize: '6rem', WebkitTextStroke: '2px black' }}>koi <Image src="/koi.png" alt="koi" width="140" height="140"/> <br></br> polloi </h1>
+
+      </div>
+
+      <div style = {{ display: 'flex', flexFlow: 'row wrap' }}>
+      {/* create game button */}
       <Link href={`/${uuidv4()}`} passHref>
-        <button>new game</button>
+        <div style={{ position: 'relative', textAlign: 'center'}}>
+        <Image src="/rock_2.png" alt="create game rock" width="140" height="40"/>
+        <p style={{ position: 'absolute', top: '-15%', left: '15%', color: 'black', fontSize: '1rem'}}>create game</p>
+        </div>
       </Link>
+
+      {/* join game button */}
+      <Link href={`/${uuidv4()}`} passHref>
+        <div style={{ position: 'relative', textAlign: 'center'}}>
+        <Image src="/rock_1.png" alt="join game rock" width="140" height="40"/>
+        <p style={{ position: 'absolute', top: '-15%', left: '25%', color: 'black', fontSize: '1rem'}}>join game</p>
+        </div>
+      </Link>
+      </div>
+
     </Container>
   )
 }
